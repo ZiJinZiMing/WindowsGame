99993b1f3a14a5bd6da43f084f8e19f0951e6bb1
ZhangJinming <zjm545237835@gmail.com>
2020-04-15 00:40:19 +0800

    添加Chapter2/demo1

---
+++ Chapter2/demo1/demo1.cpp
@@ -0,0 +1,11 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <windowsx.h>
+
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance, LPSTR lpcmdline, int ncmdshow)
+{
+	MessageBoxA(NULL, "this is content", "this is title", MB_OK);
+	return 0;
+}
